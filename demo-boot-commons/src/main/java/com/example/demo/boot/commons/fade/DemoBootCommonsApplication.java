018c65e0f80d450adc1a56141d92c8949d0a4678
zongk <ab9527@hotmail.com>
2019-07-25 14:30:39 +0800

    修改版本号

---
+++ demo-boot-commons/src/main/java/com/example/demo/boot/commons/fade/DemoBootCommonsApplication.java
@@ -0,0 +1,16 @@
+package com.example.demo.boot.commons.fade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zongkun
+ * @creed Talk is cheap,show me the code
+ * @since 2019-7-24 9:37
+ */
+@SpringBootApplication
+public class DemoBootCommonsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoBootCommonsApplication.class, args);
+    }
+}
