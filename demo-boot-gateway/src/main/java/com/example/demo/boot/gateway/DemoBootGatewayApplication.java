237e07d96465abae2b4af03ffaad4fdd14b35932
zongk <ab9527@hotmail.com>
2019-07-25 17:25:45 +0800

    添加gateway配置

--- demo-boot-gateway/src/main/java/com/example/demo/boot/gateway/DemoBootGatewayApplication.java
+++ demo-boot-gateway/src/main/java/com/example/demo/boot/gateway/DemoBootGatewayApplication.java
@@ -2,27 +2,18 @@ package com.example.demo.boot.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+/**
+ * @author zongkun
+ * @creed Talk is cheap,show me the code
+ * @since 2019-7-25 17:16
+ */
 @SpringBootApplication
+@EnableEurekaClient
 public class DemoBootGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoBootGatewayApplication.class, args);
-	}
-
-	@Bean
-	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-		return builder.routes()
-				.route("path_route", r -> r.path("/get")
-						.uri("http://httpbin.org"))
-				.route("path_route1", r -> r.path("/get1")
-						.uri("https://www.baidu.com"))
-				.route("path_route2", r -> r.path("/get")
-						.uri("http://httpbin.org"))
-				.build();
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(DemoBootGatewayApplication.class);
+    }
 }
